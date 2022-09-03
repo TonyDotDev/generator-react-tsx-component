6491178aa844eb775277168f45540b2d86aec02f
Tony Pettigrew <tonypdev@Tonys-MacBook-Pro.local>
2022-09-03 13:39:59 -0700

    test: Add mocha tests

---
+++ test/app.test.js
@@ -0,0 +1,94 @@
+const helpers = require("yeoman-test");
+const assert = require("yeoman-assert");
+const path = require("path");
+
+describe("generator:app", () => {
+  const NAME = "TsxComponent";
+  describe("Name argument with no flags", () => {
+    before((done) => {
+      helpers.run(path.join(__dirname, "../app")).withArguments([NAME]).on("end", done);
+    });
+
+    describe("[component]/index.ts", () => {
+      const filePath = `${NAME}/index.ts`;
+
+      it("is created", () => {
+        assert.file([filePath]);
+      });
+
+      it("has component export", () => {
+        const fileContent = `export * from './${NAME}'`;
+        assert.fileContent(filePath, fileContent);
+      });
+    });
+
+    describe("[component]/[component].css", () => {
+      const filePath = `${NAME}/${NAME}.css`;
+
+      it("is created", () => {
+        assert.file([filePath]);
+      });
+
+      it("is has no content", () => {
+        assert.fileContent(filePath, "");
+      });
+    });
+
+    describe("[component]/[component].tsx", () => {
+      const filePath = `${NAME}/${NAME}.tsx`;
+
+      it("is created", () => {
+        assert.file([filePath]);
+      });
+
+      it("is exports component code", () => {
+        const fileStrings = [
+          `import React from 'react';\n\n`,
+          `import './${NAME}.css';\n\n`,
+          `export function ${NAME}(): React.ReactElement {\n`,
+          `  return <div>${NAME}</div>;\n`,
+          `}`,
+        ];
+
+        assert.equalsFileContent(filePath, fileStrings.join(""));
+      });
+    });
+
+    describe("[component]/__tests__", () => {
+      it("is not created", () => {
+        const filePath = `${NAME}/__tests__/${NAME}.test.tsx`;
+        assert.noFile([filePath]);
+      });
+    });
+  });
+
+  describe("Name argument with test flag", () => {
+    before((done) => {
+      helpers
+        .run(path.join(__dirname, "../app"))
+        .withArguments([NAME])
+        .withOptions({ test: true })
+        .on("end", done);
+    });
+
+    describe("[component]/__tests__/[component].test.tsx", () => {
+      const filePath = `${NAME}/__tests__/${NAME}.test.tsx`;
+
+      it("is created", () => {
+        assert.file([filePath]);
+      });
+
+      it("has test boilerplate", () => {
+        const fileStrings = [
+          `import { render } from '@/test/test-utils';\n\n`,
+          `import { ${NAME} } from '../${NAME}';\n\n`,
+          `describe('${NAME}', () => {\n`,
+          `  render({ui: <${NAME} />});\n`,
+          `})`,
+        ];
+
+        assert.equalsFileContent(filePath, fileStrings.join(""));
+      });
+    });
+  });
+});
